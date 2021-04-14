7845150e772a36fc9b64427aa979d39eac62a23e
Joao Alejandro <joaoalejandro32@gmail.com>
2021-04-13 21:53:31 -0300

    Testes de js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const element = document.querySelector('h1')
 element.addEventListener('click', function(event){
     console.log(event)
 })
+
+var primeiraUl = document.querySelector('ul')
+
+console.log(primeiraUl.innerText)
+
+var todasImagens = document.querySelectorAll('img[src^="img/imagem"]')
+
+console.log(todasImagens)
+
