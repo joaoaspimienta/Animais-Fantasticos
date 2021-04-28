ff25d4496cd207cc32be2de3c6583d708ace24d1
joaoaspimienta <61059180+joaoaspimienta@users.noreply.github.com>
2021-04-28 13:21:32 -0300

    Terminada parte só de arrays

--- objetos.js
+++ objetos.js
@@ -1,3 +1,5 @@
+/*Funções construtoras*/
+
 //Cria função construtora de carro e depois novos objetos
 function Carro(marca, preco) {
     this.marca = marca;
@@ -58,10 +60,14 @@ function Seletor(itens){
   slaMano.removeClasse('ativo')
 
 
+/*Math random*/ 
+
 //Como selecionar um numero aleatorio entre 90 e 100
 var fodase = Math.floor(Math.random() * (100 - 90 + 1) + 90)
 console.log(fodase)
 
+
+/*Array*/
 
 //Preenche a array com a string banana desde o inicio por 3 espaços
 console.log(['item1', 'item2', 'item3', 'item4'].fill('banana', 0, 3))
@@ -75,20 +81,27 @@ transporte1.concat(transporte2, 'teste', 'oi')
 [].concat(transporte1, transporte2)
 
 
-//Verifica se o array possui o item e retorna true ou false
-[].includes(item)
-//Verifica se o array possui o item e retorna o index do primeiro item encontrado, tambem tem o [].lastIndexOf(item) que pega o ultimo item encontrado 
-[]indexOf(item)
-
+const linguagens = ['html', 'css', 'js', 'php', 'python']
 
 //Une os itens do array linguagens em uma string' e adiciona espaços entre eles
 linguagens.join(' ')
 
-
 //Cria um clone do array 
-const linguagens = ['html', 'css', 'js', 'php', 'python']
-const cloneLinguagens = linguagens.splice()
+const cloneLinguagens = linguagens.slice()
 
-//
+
+//Retorna elementos selecionados de uma array em uma nova array (inclui o argumento inicial, mas não o final, apenas os antes dele (estranho, sim, eu sei))
 linguagens.slice(3) //['php', 'python']
 linguagens.slice(1, 4) //['css', 'js', 'php']
+
+
+arrayRandom.pop() //Deleta o ultimo item do array
+arrayRandom.unshift() //adiciona item ao inicio da array
+arrayRandom.push() //adiciona item ao final da array
+arrayRandom.shift() //remove item do inicio da array
+arrayRandom.pop() //remove item do final da array
+arrayRandom.sort() //organiza por ordem alfabetica
+arrayRandom.includes('joana') //verifica se o array possui item e retorna true ou false
+arrayRandom.indexOf('joana') //verifica se o array possui item e retorna o index do primeiro item encontrado
+arrayRandom.lastIndexOf('joana') //verifica se o array possui item e retorna o index do utlimo item encontrado
+arrayRandom = arrayRandom.split('h1').join('h2') //troca os h1 por h2 por algum motivo
