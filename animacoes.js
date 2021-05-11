cac0f153431ce0f12277239fe7c1fe3497836128
Joao Alejandro <joaoalejandro32@gmail.com>
2021-05-10 21:23:44 -0300

    Todas as alterações de dataset feitas...
    Falta modules e modal

--- animacoes.js
+++ animacoes.js
@@ -1,6 +1,6 @@
 function initTabNav(){
-    const tabMenu = document.querySelectorAll('.js-tabmenu li');
-    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+    const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
     if(tabMenu.length && tabContent.length){
         tabContent[0].classList.add('ativo')
@@ -9,7 +9,8 @@ function initTabNav(){
             tabContent.forEach((section) => {
                 section.classList.remove('ativo');
             });
-            tabContent[index].classList.add('ativo');
+            const direcao = tabContent[index].dataset.anime;
+            tabContent[index].classList.add('ativo', direcao);
         }
 
         tabMenu.forEach((itemMenu, index) => {
@@ -23,7 +24,7 @@ initTabNav();
 
 
 function initAccordion() {
-    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
     const activeClass = 'ativo';
 
     if(accordionList.length){
@@ -44,7 +45,7 @@ function initAccordion() {
 initAccordion();
 
 function initScrollSuave(){
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]')
 
 function scrollToSection(event) {
     event.preventDefault();
@@ -65,7 +66,7 @@ linksInternos.forEach((link) => {
 initScrollSuave();
 
 function initAnimacaoScroll(){
-    const sections = document.querySelectorAll('.js-scroll');
+    const sections = document.querySelectorAll('[data-anime="scroll"]');
 
     if(sections.length){
         const windowMetade = window.innerHeight * 0.6;
