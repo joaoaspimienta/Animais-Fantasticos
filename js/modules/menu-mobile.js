f6fad90dc6f65fb1075aee9c3f593510e69d71cf
Joao Alejandro <joaoalejandro32@gmail.com>
2021-06-01 18:35:15 -0300

    Anima numeros finalizado

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -9,8 +9,7 @@ export default function initMenuMobile() {
     function openMenu(event) {
         menuList.classList.add('active')
         menuButton.classList.add('active')
-        outsideClick(menuList, eventos, () => {
-            
+        outsideClick(menuList, eventos, () => {            
             menuList.classList.remove('active')
             menuButton.classList.remove('active')
         })
